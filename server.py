f75ab84ce6163dd1d612c993665e84d58b38c53b
Yemin Shi <shiyemin@outlook.com>
2022-12-06 18:47:48 +0800

    fix login error

--- server.py
+++ server.py
@@ -1,65 +1,106 @@
 """Make some requests to OpenAI's chatbot"""
 
-import sys
-sys.path.append("ChatGPT/src")
-
-import time
 import os
+import time
+import json
+import threading
 from expiringdict import ExpiringDict
 
 from flask import Flask, request, jsonify, Response
 
-from revChatGPT.revChatGPT import Chatbot
-from get_auth import get_token
+from Classes import auth as Auth
+from PyChatGPT.Classes import chat as Chat
+
+# Fancy stuff
+import colorama
+from colorama import Fore
 
 
-MAX_SESSION_NUM = 300
+MAX_SESSION_NUM = 3000
 MAX_AGE_SECONDS = 1800
 
 APP = Flask(__name__)
 
-config = get_token()
-if config is None:
-    sys.exit(1)
+colorama.init(autoreset=True)
 
-bot_cache = ExpiringDict(max_len=MAX_SESSION_NUM, max_age_seconds=MAX_AGE_SECONDS)
+# Check if config.json exists
+if not os.path.exists("config.json"):
+    print(">> config.json is missing. Please create it.")
+    print(f"{Fore.RED}>> Exiting...")
+    exit(1)
 
-def get_chatbot(user):
-    if user not in bot_cache:
-        chatbot = Chatbot(config, conversation_id=None)
-        if 'session_token' in config:
-            chatbot.refresh_session()
-        bot_cache[user] = chatbot
-    bot = bot_cache[user]
-    bot_cache[user] = bot
-    return bot
+with open("config.json", "r") as f:
+    config = json.load(f)
+    # Check if email & password are in config.json
+    if "email" not in config or "password" not in config:
+        print(">> config.json is missing email or password. Please add them.")
+        print(f"{Fore.RED}>> Exiting...")
+        exit(1)
+
+# Try login
+expired_creds = Auth.expired_creds()
+sem = threading.Semaphore()
+def try_login():
+    sem.acquire()
+    if Auth.expired_creds():
+        open_ai_auth = Auth.LocalOpenAIAuth(email_address=config["email"], password=config["password"])
+        print(f"{Fore.GREEN}>> Credentials have been refreshed.")
+        open_ai_auth.begin()
+        time.sleep(3)
+    sem.release()
+    return Auth.get_access_token()
+
+print(f"{Fore.GREEN}>> Checking if credentials are expired...")
+if expired_creds:
+    print(f"{Fore.RED}>> Your credentials are expired." + f" {Fore.GREEN}Attempting to refresh them...")
+    access_token = try_login()
+    is_still_expired = Auth.expired_creds()
+    if is_still_expired:
+        print(f"{Fore.RED}>> Failed to refresh credentials. Please try again.")
+        exit(1)
+    else:
+        print(f"{Fore.GREEN}>> Successfully refreshed credentials.")
+else:
+    print(f"{Fore.GREEN}>> Your credentials are valid.")
+
+# Cache all conv id
+prev_conv_id_cache = ExpiringDict(max_len=MAX_SESSION_NUM, max_age_seconds=MAX_AGE_SECONDS)
+def get_prev_conv_id(user):
+    if user not in prev_conv_id_cache:
+        prev_conv_id_cache[user] = None
+    prev_conv_id = prev_conv_id_cache[user]
+    return prev_conv_id
+
+def set_prev_conv_id(user, prev_conv_id):
+    prev_conv_id_cache[user] = prev_conv_id
+
 
 @APP.route("/chat", methods=["POST"])
 def chat():
+    global access_token
+
     message = request.json["message"].strip()
     user = request.json["user"].strip()
+    prev_conv_id = get_prev_conv_id(user)
     print(f"{user} message: {message}")
-    try:
-        bot = get_chatbot(user)
-        if message == "reset":
-            bot.reset_chat()
-            response = "done"
-        else:
-            response = bot.get_chat_response(message, output="text")
-            if isinstance(response, Exception):
-                raise response
-            response = response["message"]
-    except Exception as e:
-        print(f"Error: ", e)
+    answer, previous_convo = Chat.ask(auth_token=access_token,
+                                      prompt=message,
+                                      previous_convo_id=prev_conv_id)
+    if answer == "400" or answer == "401":
+        print(f"{Fore.RED}>> Your token is invalid. Attempting to refresh..")
+        access_token = try_login()
         return Response(
-                str(e),
+                "Your token is invalid. Please try again.",
                 status=400,
             )
-    print(f"Response to {user}: {response}")
-    return jsonify({"response": response}), 200
+
+    print(f"Response to {user}: {answer}")
+    if previous_convo is not None:
+        set_prev_conv_id(user, previous_convo)
+    return jsonify({"response": answer}), 200
 
 def start_browser():
-    APP.run(port=5000, threaded=True)
+    APP.run(port=5001, threaded=True)
 
 if __name__ == "__main__":
     start_browser()
